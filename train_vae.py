7330b4e70cbdc9058bb2ec93c3578906328eded4
PROLCY <ckd248@naver.com>
2024-04-27 14:08:37 +0000

    change pose geneartion process

--- train_vae.py
+++ train_vae.py
@@ -95,7 +95,7 @@ def train():
 
     cfg = Config()
     model = VariationalAutoencoder(cfg).to(device)
-    model.load_state_dict(torch.load('checkpoints/experiment12/model_63_1929_1433.pt'))
+    # model.load_state_dict(torch.load('checkpoints/experiment12/model_63_1929_1433.pt'))
 
     def vae_loss(recon_x, x, mu, logvar):
 
@@ -158,14 +158,16 @@ def train():
 def generate_pose(base_pose, scale, deformation, target_point):
     target_point_method = Config().target_point_method
     scaled_cluster = base_pose * scale.unsqueeze(1)
+    deformed_pose = scaled_cluster + deformation.view(-1, 16, 2)
     if target_point_method == 'mean':
-        center_position = torch.mean(scaled_cluster, dim=1)
+        center_position = torch.mean(deformed_pose, dim=1)
     if target_point_method == 'center':
-        max_values, _ = torch.max(scaled_cluster, dim=1)
-        min_values, _ = torch.min(scaled_cluster, dim=1)
+        max_values, _ = torch.max(deformed_pose, dim=1)
+        min_values, _ = torch.min(deformed_pose, dim=1)
         center_position = torch.stack([(max_values[:, 0] - min_values[:, 0]) / 2, (max_values[:, 1] - min_values[:, 1]) / 2], dim=1)
-    positioned_pose = scaled_cluster + (target_point - center_position).unsqueeze(1)
-    generated_pose = positioned_pose + deformation.view(-1, 16, 2)
+    if type(target_point_method) == int:
+        center_position = deformed_pose[:, target_point_method, :]
+    generated_pose = deformed_pose + (target_point - center_position).unsqueeze(1)
 
     return generated_pose
 
