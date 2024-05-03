b195820588b11e4acbaeede8e34ff6a455cfbdbb
Seunggi Moon <moon44432@naver.com>
2024-05-03 17:34:59 +0900

    MPII -> COCO pose conversion

--- vis_pose.py
+++ vis_pose.py
@@ -21,6 +21,78 @@ point_color = [(255,0,0),(0,255,0),(0,0,255),
                (128,255,0),(128,128,128),(255,128,0),
                (255,0,128),(255,255,255)]
 
+
+coco_link_pairs = [[0, 1], [1, 2], [2, 3], 
+              [3, 4], [1, 5], [5, 6], 
+              [6, 7], [1, 8], [8, 9], 
+              [9, 10], [1, 11], [11, 12],
+              [12, 13],[0, 14],[14, 16], [0, 15], [15, 17]]
+
+coco_link_color = [(0, 0, 255), (0, 0, 255), (0, 0, 255),
+              (0, 255, 0), (0, 255, 0), (0, 255, 0),
+              (0, 255, 255), (0, 0, 255), (0, 0, 255),
+              (0, 0, 255), (0, 255, 0), (0, 255, 0),
+              (0, 255, 0), (0, 255, 255), (0, 255, 255), (128, 128, 0), (128, 0, 128)]
+
+coco_point_color = [(255,0,0),(0,255,0),(0,0,255), 
+               (128,0,0), (0,128,0), (0,0,128),
+               (255, 255, 0),(0,255,255),(255, 0, 255),
+               (128,128,0),(0, 128, 128),(128,0,128),
+               (128,255,0),(128,128,128),(255,128,0),
+               (255,0,128),(255,255,255), (128, 128, 0), (128, 0, 128)]
+
+'''
+original pose:
+2 1 0 : right leg
+3 4 5 : left leg
+12 11 10 : right arm
+13 14 15 : left arm
+7 : neck
+9 8 : head
+
+coco:
+0 : nose
+1 : neck
+2 3 4 : right arm
+5 6 7 : left arm
+8 9 10 : right leg
+11 12 13 : left leg
+14 15 : eyes
+16 17 : ears
+'''
+
+def convert_pose(pose):
+    def get_length(p1, p2):
+        return ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**0.5
+
+    coco = []
+    
+    nose = ((pose[9][0] + pose[8][0]) // 2, (pose[9][1] + pose[8][1]) // 2)
+    eye_center = ((pose[9][0] * 6 + pose[8][0] * 4) // 10, (pose[9][1] * 6 + pose[8][1] * 4) // 10)
+    coco.append(nose)
+    coco.append(pose[7])
+    coco += reversed(pose[10:13])
+    coco += pose[13:16]
+    coco += reversed(pose[0:3])
+    coco += pose[3:6]
+
+    ear_dist = ((coco[2][0] - coco[5][0]) // 4, (coco[2][1] - coco[5][1]) // 4)
+
+    xdelta = (coco[0][0] - coco[1][0]) - (coco[1][0] - (coco[11][0] + coco[8][0]) // 2) // 10
+
+    collar_vec = (coco[2][0] - coco[1][0], coco[2][1] - coco[1][1])
+    neck_vec = (coco[0][0] - coco[1][0], coco[0][1] - coco[1][1])
+
+    ydelta = collar_vec[0] * neck_vec[0] + collar_vec[1] * neck_vec[1]
+
+    coco.append(((eye_center[0] + ear_dist[0] // 3 - xdelta // 3), (eye_center[1] + ear_dist[1] // 3 - ydelta // 100)))
+    coco.append(((eye_center[0] - ear_dist[0] // 3 - xdelta // 3), (eye_center[1] - ear_dist[1] // 3 - ydelta // 100)))
+    coco.append(((coco[0][0] + ear_dist[0] - xdelta // 2), (coco[0][1] + ear_dist[1] - ydelta // 150)))
+    coco.append(((coco[0][0] - ear_dist[0] - xdelta // 2), (coco[0][1] - ear_dist[1] - ydelta // 150)))
+
+    return coco
+
+
 def vis_pose(image_path, pose_keypoints, show=True):
     image = cv2.imread(image_path)
 
@@ -28,6 +100,7 @@ def vis_pose(image_path, pose_keypoints, show=True):
         cv2.line(image, pose_keypoints[pair[0]], pose_keypoints[pair[1]], link_color[idx], 2)
 
     for idx, point in enumerate(pose_keypoints):
+        cv2.putText(image,str(idx),point,cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
         if idx != 16:
             cv2.circle(image, point, 5, point_color[idx], thickness=-1)
         else:
@@ -37,7 +110,25 @@ def vis_pose(image_path, pose_keypoints, show=True):
         cv2.imshow("image", image)
         cv2.moveWindow("image", 0, 0)
         cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
+    return image
+
+def vis_pose_coco(image_path, pose_keypoints, show=True):
+    image = cv2.imread(image_path)
+
+    pose_keypoints = convert_pose(pose_keypoints)
+    for idx, pair in enumerate(coco_link_pairs):
+        cv2.line(image, pose_keypoints[pair[0]], pose_keypoints[pair[1]], coco_link_color[idx], 2)
+
+    for idx, point in enumerate(pose_keypoints):
+        cv2.putText(image,str(idx),point,cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
+        cv2.circle(image, point, 5, coco_point_color[idx], thickness=-1)
+
+    if show:
+        cv2.imshow("image", image)
+        cv2.moveWindow("image", 0, 0)
+        cv2.waitKey(0)
+    cv2.destroyAllWindows()
     return image
 
 def vis_pose_data(data):
@@ -51,6 +142,7 @@ def vis_pose_data(data):
     for i in range(0, len(pose_data), 2):
         pose_keypoints.append((pose_data[i], pose_data[i+1]))
     vis_pose(image_path, pose_keypoints)
+    vis_pose_coco(image_path, pose_keypoints)
 
 if __name__ == '__main__':
     predicted = [(595.5921267553274, 404.10781700000007), (587.4540269712579, 375.26168899999993), (674.1323874006324, 382.447135), (709.0841106363025, 390.41310699999985), (738.5057531861758, 386.341047), (664.3825897951702, 417.24250400000005), (693.034285958547, 387.2644009999999), (714.1887366306264, 310.66663199999994), (711.364003176137, 296.50660700000003), (702.7690875859935, 228.0), (630.6202741684986, 387.9697880000001), (647.1081440971391, 359.6237799999999), (670.0994861445556, 309.9926839999998), (761.1125559742334, 314.1373520000001), (758.0154726576313, 389.9528070000001), (678.8184304028331, 402.0585960000001)]
@@ -59,7 +151,7 @@ if __name__ == '__main__':
 
     vis_pose(os.path.join('./affordance_data/data', 'ELR_matches/S02/E0012.mkv/frame_00002381.jpg'), predicted + [(0, 0)])
     vis_pose(os.path.join('./affordance_data/data', 'ELR_matches/S02/E0012.mkv/frame_00002381.jpg'), gt + [(0, 0)])
-    input()
+    vis_pose_coco(os.path.join('./affordance_data/data', 'ELR_matches/S02/E0012.mkv/frame_00002381.jpg'), gt + [(0, 0)])
 
     train_data_path = './affordance_data/trainlist.txt'
     train_data = []
